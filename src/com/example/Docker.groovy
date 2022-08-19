8b07a4294daa2b357cadb8e8009de8f84489f8e7
SHALINDERJEET SINGH <gurshanbir@gmail.com>
2022-08-19 16:28:34 +1000

    ok

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,18 @@
+#!/usr/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+    def script
+
+    Docker(script){
+        this.script=script
+    }
+    def buildImage(String imageName){
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "docker build -t $imageName ."
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+            script.sh "docker push $imageName"
+        }
+    }
+}
+
--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,6 @@
 #!/usr/bin/env groovy
+import com.example.Docker
 
-def call() {
-    withCredentials([usernamePassword(credentialsId: 'docker-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t shalinder/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push shalinder/demo-app:jma-2.0'
-    }
+def call(String imageName) {
+return new Docker(this).buildImage(imageName)
 }
